a88af4bb27a60a4f15cbf905c3553bbda37ac9a8
Rakshit Garg <raks.garg@gmail.com>
2014-08-14 17:30:18 -0700

    Initial Commit

---
+++ Unplug/ViewController.h
@@ -0,0 +1,40 @@
+//
+//  ViewController.h
+//  Unplug
+//
+//  Created by Gargium on 8/12/14.
+//  Copyright (c) 2014 Gargium Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
+
+
+@interface ViewController : UIViewController {
+    
+    CMMotionManager *motionManager;
+    NSOperationQueue *queue;
+}
+
+@property (weak, nonatomic) IBOutlet UIProgressView *xAccelBar;
+@property (weak, nonatomic) IBOutlet UIProgressView *yAccelBar;
+@property (weak, nonatomic) IBOutlet UIProgressView *zAccelBar;
+
+@property (weak, nonatomic) IBOutlet UILabel *xAccelLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yAccelLabel;
+@property (weak, nonatomic) IBOutlet UILabel *zAccelLabel;
+
+@property (weak, nonatomic) IBOutlet UIProgressView *xGyroBar;
+@property (weak, nonatomic) IBOutlet UIProgressView *yGyroBar;
+@property (weak, nonatomic) IBOutlet UIProgressView *zGyroBar;
+
+@property (weak, nonatomic) IBOutlet UILabel *xGyroLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yGyroLabel;
+@property (weak, nonatomic) IBOutlet UILabel *zGyroLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *xMagnetLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yMagnetLabel;
+@property (weak, nonatomic) IBOutlet UILabel *zMagnetLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalMagnetLabel;
+
+@end
---
+++ Unplug/ViewController.m
@@ -0,0 +1,93 @@
+//
+//  ViewController.m
+//  Unplug
+//
+//  Created by Gargium on 8/12/14.
+//  Copyright (c) 2014 Gargium Interactive. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+
+@implementation ViewController
+@synthesize xAccelBar, yAccelBar, zAccelBar, xAccelLabel, yAccelLabel, zAccelLabel;
+@synthesize xGyroBar, yGyroBar, zGyroBar, xGyroLabel, yGyroLabel,
+    zGyroLabel;
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    motionManager = [[CMMotionManager alloc] init];
+    motionManager.accelerometerUpdateInterval = 1.0/10.0; //Update at 10Hz
+    
+    if (motionManager.accelerometerAvailable) {
+        NSLog(@"Accelerometer Available");
+        queue = [NSOperationQueue currentQueue];
+        
+        [motionManager startAccelerometerUpdatesToQueue:queue
+                withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
+                    CMAcceleration acceleration = accelerometerData.acceleration;
+                    xAccelLabel.text = [NSString stringWithFormat:@"%f", acceleration.x];
+                    xAccelBar.progress = ABS(acceleration.x);
+                    
+                    yAccelLabel.text = [NSString stringWithFormat:@"%f", acceleration.y];
+                    yAccelBar.progress = ABS(acceleration.y);
+                    
+                    zAccelLabel.text = [NSString stringWithFormat:@"%f", acceleration.z];
+                    zAccelBar.progress = ABS(acceleration.z);
+                    
+                }];
+    }
+    
+    motionManager.gyroUpdateInterval = 1.0/2.0; //Update every 1.2 seconds
+        
+    if(motionManager.gyroAvailable) {
+        NSLog(@"Gyro Available");
+        queue = [NSOperationQueue currentQueue];
+            
+        [motionManager startGyroUpdatesToQueue:queue
+                withHandler:^(CMGyroData *gyroData, NSError *error) {
+                    CMRotationRate rotate = gyroData.rotationRate;
+                    xGyroLabel.text = [NSString stringWithFormat:@"%f",rotate.x];
+                    xAccelBar.progress = ABS(rotate.x);
+                    
+                    yGyroLabel.text = [NSString stringWithFormat:@"%f",
+                                       rotate.y];
+                    yAccelBar.progress = ABS(rotate.y);
+                    
+                    zGyroLabel.text = [NSString stringWithFormat:@"%f",
+                                       rotate.z];
+                    zAccelBar.progress = ABS(rotate.z);
+                    
+                }];
+    }
+    
+    motionManager.magnetometerUpdateInterval = 1.0/10.0; //update at 10 hz
+    if (motionManager.magnetometerAvailable) {
+        queue = [NSOperationQueue currentQueue];
+        
+        [motionManager startMagnetometerUpdatesToQueue:queue withHandler:^(CMMagnetometerData *magnetometerData, NSError *error) {
+            
+            CMMagneticField field = magnetometerData.magneticField;
+            self.xMagnetLabel.text = [NSString stringWithFormat:@"%f", field.x];
+            self.yMagnetLabel.text = [NSString stringWithFormat:@"%f", field.y];
+            self.zMagnetLabel.text = [NSString stringWithFormat:@"%f", field.z];
+            self.totalMagnetLabel.text = [NSString stringWithFormat:@"%f", sqrt(field.x * field.x + field.y * field.y + field.z * field.z)];
+        }];
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
